aaa96852134590cbc2d1d5b0dd51619cd67159d8
Joe21 <joej21@gmail.com>
2013-10-14 14:37:57 -0400

    removed initializers changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,9 +256,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-
-  # Joe's changes
-  config.http_authenticatable_on_xhr = false
-  config.navigational_formats = ["*/*", :html, :json]
-
 end
