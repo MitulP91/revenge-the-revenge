f8707aebcdcbcb23682f1a511202a9ae0678412c
Joe21 <joej21@gmail.com>
2013-10-14 12:00:05 -0400

    adding seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,9 @@ User.delete_all
 Score.delete_all
 Comment.delete_all
 
-# User.create(email: "somedude@dude.com", password: "password")
+nastyboy = User.create(email: "nastyboy@andrewmadden.com", password: "password", password_confirmation: "password")
+comment1 = Comment.create(the_comment: "This game is ill nasty!")
+score1 = Score.create(the_score: 9000)
+
+nastyboy.comments << comment1
+nastyboy.scores << score1
