99eff987b83c83788f5708fc661be2ddac8a0977
MitulP91 <mitulp91@gmail.com>
2013-10-14 11:19:44 -0400

    fix spec

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -4,18 +4,18 @@ describe Score do
 	before :each do
 		@the_score = 5000
 		@user_id = 22
-		@score = Score.create(the_score: @the_score, user_id: @user_id)
+		@score = Score.new(the_score: @the_score, user_id: @user_id)
 	end
 
 	describe '#the_score' do
 		it 'should return the score' do
-			@score.the_score.should eq(@the_score);
+			@score.the_score.should eq(@the_score)
 		end
 	end
 
 	describe '#user_id' do
 		it 'should return the user_id of the score' do
-			@score.user_id.should eq(@user_id);
+			@score.user_id.should eq(@user_id)
 		end
 	end
 end
