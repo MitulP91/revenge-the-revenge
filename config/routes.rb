fe19b2cad2794d84c4d96656eaa36ceee9bc3359
MitulP91 <mitulp91@gmail.com>
2013-10-14 14:06:06 -0400

    undo stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 AndrewmaddenCom::Application.routes.draw do
 
-  
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
   devise_for :users
-
   root to: 'projects#index'
-
 
 end
