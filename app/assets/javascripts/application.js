945f5a2c90e5d300c766a2ee44507cb0e7ad2bfe
Joe21 <joej21@gmail.com>
2013-10-14 17:05:43 -0400

    devise signin ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function fader() {
+	$('#darth-fader').css("background-color", "green");
+}
+
+$(function(){
+	// var $signin_button = $('#signin-button');
+	// $signin_button.on("click", function(){this.fadeIn(2000)});
+
+	$('#sign-in-form').on('submit', function(){
+		
+		var email = $('#user_email').val();
+		var password = $('#user_password').val();
+		
+		var data = {remote: true, commit: "Sign in", utf8: "✓", user: {remember_me: 1, password: password, email: email}};
+		$.ajax({
+			url: '/users/sign_in',
+			data: data,
+			method:'POST',
+			dataType:'json'
+		})
+		.done(function(data){
+			if (data['success']){
+				$('body').css({'backgroundColor': 'green'});
+			}
+			else{
+				$('body').css({'backgroundColor': 'red'});
+			}
+		});
+		.fail(function(){
+			$('body').css({'backgroundColor': 'red'});
+		});
+		return false;
+	});
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AndrewmaddenCom::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => 'sessions'}
   root to: 'projects#index'
 
 
