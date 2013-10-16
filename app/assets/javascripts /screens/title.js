a3814bdc208ac123009b589747277eb21c5049a8
CassieMichelle <cassie.walther@gmail.com>
2013-10-16 11:49:48 -0400

    broken words

--- app/assets/javascripts /screens/title.js
+++ app/assets/javascripts /screens/title.js
@@ -9,12 +9,27 @@ game.TitleScreen = me.ScreenObject.extend({
 		this.scrollerfont = null;
 		this.scrollertween = null;
 
-		this.scroller = 'MITUL PATEL MADE A GAME but Cassie created the greatest wall ';
-		this.scrollpos = 600;
+		///test scroll up
+		this.storySize = 18;
+		this.storyX = 300;
+		this.storyY = -750;
+
+
+    	//end
+
+
+
+    	this.scroller = 'MITUL PATEL MADE A GAME CASSIE CREATED THE GREATEST WALL ';
+		var scroller = this.scroller 
 	},
 
 	onResetEvent: function() {
 		// If null, initialize the variables
+		this.storySize = 18;
+		this.storyX = 300;
+		this.storyY = -750;
+
+
 		if(this.title == null) {
 			// Initialize background image
 			this.title = me.loader.getImage('title_screen');
@@ -26,13 +41,21 @@ game.TitleScreen = me.ScreenObject.extend({
 			this.scrollerfont = new me.BitmapFont('32x32_font', 32);
 		}
 
-		// Reset to default
-		this.scrollerpos = 640;
+		// // Reset to default
+		// this.scrollerpos = 640;
 
-		// Tween to Animate Arrow
-    	this.scrollertween = new me.Tween(this).to({
-        	scrollerpos: -850
-    	}, 10000).onComplete(this.scrollover.bind(this)).start();
+		// // Tween to Animate Arrow
+  //   this.scrollertween = new me.Tween(this).to({
+  //       scrollerpos: -850
+  //   	}, 10000).onComplete(this.scrollover.bind(this)).start();
+
+    	
+
+		var tween = new me.Tween(this).to({
+	        storySize: 0,
+	        storyX: 300,
+	        storyY: 0
+	    }, 500).onComplete(this.scrollover.bind(this)).start();;
 
 		// Enable the Keyboard
 		me.input.bindKey(me.input.KEY.ENTER, 'enter', true);
@@ -44,11 +67,29 @@ game.TitleScreen = me.ScreenObject.extend({
 	// Tween Callback Function
 	scrollover: function() {
 		// Reset to Default
-		this.scrollerpos = 640;
-		this.scrollertween.to({
-			scrollerpos: -2200
-		},10000).onComplete(this.scrollover.bind(this)).start();
+		// this.scrollerpos = 640;
+		// 	this.scrollertween.to({
+		// 		scrollerpos: -2200
+		// 	},10000).onComplete(this.scrollover.bind(this)).start();
+		// },
+
+		
+	this.storySize = 18;
+ 	this.storyX = 300;
+	this.storyY = -700;
+
+
+	this.tween.to( {
+	       storySize: 0,
+	        storyX: 300,
+	         storyY: 0
+	     }, 500)
+	     .start();
+
 	},
+
+	
+
 
 	update: function() {
 		// If pressed
@@ -60,16 +101,24 @@ game.TitleScreen = me.ScreenObject.extend({
 
 	draw: function(context) {
 		context.drawImage(this.title, 0, 0);
+		this.scroller = 'MITUL PATEL MADE A GAME but Cassie created the greatest wall ';
 
 		this.font.draw(context, "PRESS ENTER TO PLAY", 20, 240);
-		this.scrollerfont.draw(context, this.scroller, this.scrollerpos, 440);
+		// this.scrollerfont.draw(context, this.scroller, this.scrollerpos, 440);
+		this.scrollerfont.set('century gothic', this.storySize, 'black');
+		
+		
+		this.scrollerfont.draw(context, this.scroller, this.storyY, this.storyX); 	
+		
+		
 	},
 
 	onDestroyEvent: function() {
 		// Unbind the key
 		me.input.unbindKey(me.input.KEY.ENTER);
+	}
 
 		// Just in Case
-		this.scrollertween.stop();
-	}
+		// this.scrollertween.stop
+		// this.tween.stop	}
 });
