c64376d38b014ce19c57947167904e694102b601
Joe21 <joej21@gmail.com>
2013-10-16 19:49:18 -0400

    changed enemyentity2

--- app/assets/javascripts /game.js
+++ app/assets/javascripts /game.js
@@ -58,6 +58,7 @@ var game = {
         me.entityPool.add("mainPlayer", game.PlayerEntity);
         me.entityPool.add("CoinEntity", game.CoinEntity);
         me.entityPool.add("EnemyEntity", game.EnemyEntity);
+        me.entityPool.add("EnemyEntity2", game.EnemyEntity2);
         // me.entityPool.add("level2", game.LevelEntity);
 
         // Enable Keyboard Bindings
