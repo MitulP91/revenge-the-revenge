fe851f7793e401defe5f1561fe6b2ab959a8627c
Joe21 <joej21@gmail.com>
2013-10-17 14:26:28 -0400

    added css and some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,22 @@ AndrewmaddenCom::Application.routes.draw do
 
   root to: 'projects#index'
 
-#         new_user_session GET    /users/sign_in(.:format)       sessions#new
-#             user_session POST   /users/sign_in(.:format)       sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)      sessions#destroy
-#            user_password POST   /users/password(.:format)      devise/passwords#create
-#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
-#                          PUT    /users/password(.:format)      devise/passwords#update
-# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
-#        user_registration POST   /users(.:format)               devise/registrations#create
-#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
-#                          PUT    /users(.:format)               devise/registrations#update
-#                          DELETE /users(.:format)               devise/registrations#destroy
-#                     root        /                              projects#index
-
+#         new_user_session GET    /users/sign_in(.:format)        sessions#new
+#             user_session POST   /users/sign_in(.:format)        sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)       sessions#destroy
+#            user_password POST   /users/password(.:format)       devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)   devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)  devise/passwords#edit
+#                          PUT    /users/password(.:format)       devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)         devise/registrations#cancel
+#        user_registration POST   /users(.:format)                devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)        devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)           devise/registrations#edit
+#                          PUT    /users(.:format)                devise/registrations#update
+#                          DELETE /users(.:format)                devise/registrations#destroy
+#           comment_create POST   /comment/create(.:format)       projects#create
+#             score_create POST   /score/create(.:format)         projects#scores
+#                my_scores GET    /my_scores(.:format)            projects#profile
+#              high_scores GET    /projects/high_scores(.:format) projects#high_scores
+#                     root        /                               projects#index
 end
